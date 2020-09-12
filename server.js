603fdaaee6681c826e1888e51541972350ce453f
David Magnuson <david@albus.local>
2020-09-12 14:56:59 -0500

    initialize graphql and basic schema

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const express = require('express')
+var { graphqlHTTP } = require('express-graphql')
+const schema = require('./schema/schema')
 
-const server = express()
+const server = express() // initialize server
 
-
+// express-graphql middleware
+server.use('/graphql', graphqlHTTP({
+    schema,
+    graphiql: true,
+  }))
 
 const PORT = 4000 || process.env.PORT // setting port to 4000 or 'PORT' envirmonment var (use in prod)
-server.listen(PORT, () => console.log(`listening on http://localhost:${PORT}`)) 
+server.listen(PORT, () => console.log(`listening on http://localhost:${PORT}/graphql`)) 
