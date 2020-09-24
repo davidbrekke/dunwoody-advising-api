d5c5ac0b180f1c4030e487cb518c4fe6e233f974
David Magnuson <david@albus.local>
2020-09-24 16:28:44 -0500

    apollo resolvers

---
+++ resolvers.js
@@ -0,0 +1,95 @@
+const courses = [
+    {
+        id: 1,
+        code: 'SENG3210',
+        name: 'Software Architecture',
+        credits: 3,
+        required: true,
+        instructors: ['Joe Axberg'],
+        category: 'technical',
+        instructionType: ['lecture', 'lab']
+    },
+    {
+        id: 2,
+        code: 'SENG3110',
+        name: 'Software Design',
+        credits: 4,
+        required: true,
+        instructors: ['LeAnn Simonson'],
+        category: 'technical',
+        instructionType: ['lecture', 'lab']
+    },
+    {
+        id: 3,
+        code: 'SENG3300',
+        name: 'Data Introduction',
+        credits: 3,
+        required: true,
+        instructors: ['Paula Merns'],
+        category: 'technical',
+        instructionType: ['lecture', 'lab']
+    },
+    {
+        id: 4,
+        code: 'SENG3310',
+        name: 'Database Systems',
+        credits: 4,
+        required: true,
+        instructors: ['LeAnn Simonson'],
+        category: 'technical',
+        instructionType: ['lecture', 'lab']
+    },
+]
+/* 
+const programs = [
+    {
+        id: 1,
+        code: 'SENG',
+        name: 'Software Engineering',
+        credits: 121,
+    }
+]
+const terms = [
+    {
+        id: 1,
+        season: 'fall',
+        year: 2020,
+        courses: [courses[0], courses[1], courses[2]]
+    }
+]
+const academicPlans = [
+    {
+        id: 1,
+        name: 'Software Engineering',
+        program: programs[0],
+        approvalStatus: 'n/a',
+        terms: [terms[0]]
+    }
+]
+const students = [
+    {
+        academicPlans: [academicPlans[0]],
+        program: programs[0]
+    }
+]
+const users = [
+    {
+        id: 1,
+        first_name: 'David',
+        last_name: 'Magnuson',
+        email: 'magdavj@dunwoody.edu',
+        password: '1234',
+        role: students[0]
+    }
+]
+*/
+
+
+
+const resolvers = {
+    Query: {
+        courses: () => courses,
+    },
+};
+
+module.exports = resolvers;
