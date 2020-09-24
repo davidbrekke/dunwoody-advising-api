d4c8e737d9ece089b9401771896d44560af744b9
David Magnuson <david@albus.local>
2020-09-24 16:28:00 -0500

    changed to apollo server instead of express

--- server.js
+++ server.js
@@ -1,14 +1,12 @@
-const express = require('express')
-var { graphqlHTTP } = require('express-graphql')
-const schema = require('./schema/schema')
+// using apollo for graphql server instead of express and graphql-express, see -> https://www.apollographql.com
+const { ApolloServer } = require('apollo-server'); 
+const typeDefs = require('./schema');
+const resolvers = require('./resolvers');
 
-const server = express() // initialize server
+// creating server and passing in our type definitions from schema file
+const server = new ApolloServer({ typeDefs, resolvers, playground: true, introspection: true });
 
-// express-graphql middleware
-server.use('/graphql', graphqlHTTP({
-    schema,
-    graphiql: true,
-  }))
-
-const PORT = 4000 || process.env.PORT // setting port to 4000 or 'PORT' envirmonment var (use in prod)
-server.listen(PORT, () => console.log(`listening on http://localhost:${PORT}/graphql`)) 
+// server listen
+server.listen().then(({ url }) => {
+    console.log(`🚀  Server ready at ${url}`);
+  });
